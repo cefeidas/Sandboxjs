c9ae89db29ae0915e08105d899407b58b2732fb4
cefeidas <danmarmon1986@gmail.com>
2023-03-04 23:33:02 +0000

    thre pages, thre scripts, Oh my God!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,33 @@
-function showSlides(myArray) {
-        for (let i=0; i < myArray.length; i++) {
-            myArray[i].style.display = 'none';
-        }
-        slideIndex++;
-        if(slideIndex > myArray.length) {
-            slideIndex = 1;
-        }
-        myArray[slideIndex-1].style.display = 'block'
-        setTimeout(() => showSlides(myArray), 1000) // Use an arrow function to pass the myArray argument to setTimeout
-    } 
+function showSlidesLoop(myArray) {
+    for (let i=0; i < myArray.length; i++) {
+        myArray[i].style.display = 'none';
+    }
+    slideIndex++;
+    if(slideIndex > myArray.length) {
+        slideIndex = 1;
+    }
+    myArray[slideIndex-1].style.display = 'block'
+    setTimeout(() => showSlidesLoop(myArray), 1000) 
+}
 
-let slides = document.querySelectorAll('.slides');
-window.addEventListener("load", () => showSlides(Array.from(slides)));
-
-
-function muteOrUnmute(l) {
-    for (let element = 0; element < l.length; element++) {
-        if (l[element].muted == true) {
-            l[element].muted = false;
-        } else {
-            l[element].muted = true;
-        }}};
-
-
-let audioFiles = document.querySelectorAll('audio');
-let button = document.getElementById("button");
+function muteOrUnmute(audioFiles) {
+  for (let i = 0; i < audioFiles.length; i++) {
+    audioFiles[i].muted = !audioFiles[i].muted;
+  }
+}
 
 let slideIndex = 0;
+let slides = document.querySelectorAll('.slides');
+let slidesIntro = document.querySelectorAll('.slidesIntro');
+let audioFiles = document.querySelectorAll('audio');
+let button = document.getElementById("button");
+let intro1 = document.getElementById("intro1");
+let intro2 =document.getElementById("intro2");
 
-button.addEventListener("click", () => muteOrUnmute(audioFiles));
+
+
+window.addEventListener("load", () => showSlidesLoop(Array.from(slides)));
 window.addEventListener("load", (event) => {
-    alert("For a better experience, please unmute and use a wide screen");
-  });
+alert("For a better experience, please unmute and use a wide screen");
+});
+button.addEventListener("click", () => muteOrUnmute(audioFiles));
