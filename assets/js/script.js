35ef17dad9ece3868187ec89f8481b1b0f52455d
cefeidas <danmarmon1986@gmail.com>
2023-03-04 14:13:43 +0000

    it is alive!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,34 @@
+function showSlides(myArray) {
+        for (let i=0; i < myArray.length; i++) {
+            myArray[i].style.display = 'none';
+        }
+        slideIndex++;
+        if(slideIndex > myArray.length) {
+            slideIndex = 1;
+        }
+        myArray[slideIndex-1].style.display = 'block'
+        setTimeout(() => showSlides(myArray), 1000) // Use an arrow function to pass the myArray argument to setTimeout
+    } 
 
-addEventListener("load",() => {
-    var index = 0;
-    const slides = document.querySelectorAll(".slides");
-    const classHide = "slides-hidden", count = slides.length;
-    nextSlide();
-    function nextSlide() {
-        slides[(index ++) % count].classList.add(classHide);
-        slides[index % count].classList.remove(classHide);
-        setTimeout(nextSlide, 700);
-    }
-});
+let slides = document.querySelectorAll('.slides');
+window.addEventListener("load", () => showSlides(Array.from(slides)));
 
-function muteOrUnmute(myArray) {
-    for (let element = 0; element < myArray.length; element++ ) {
-        if (myArray[element].muted == true) {
-            myArray[element].muted = false;
+
+function muteOrUnmute(l) {
+    for (let element = 0; element < l.length; element++) {
+        if (l[element].muted == true) {
+            l[element].muted = false;
         } else {
-            myArray[element].muted = true;
+            l[element].muted = true;
         }}};
-   
-    
+
+
 let audioFiles = document.querySelectorAll('audio');
 let button = document.getElementById("button");
 
-button.addEventListener("click", () => muteOrUnmute(audioFiles));
+let slideIndex = 0;
 
+button.addEventListener("click", () => muteOrUnmute(audioFiles));
 window.addEventListener("load", (event) => {
     alert("For a better experience, please unmute and use a wide screen");
   });
